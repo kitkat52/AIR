acfb7c05017ee56bf200f9130e37303e0c60cd07
Ekaterina Boltenkova <kataretta@gmail.com>
2019-04-18 18:21:48 +0300

    Initial commit

---
+++ loc_utils.py
@@ -0,0 +1,20 @@
+# Reference 1D sense and move functions
+
+def sense(p, Z):
+    q=[]
+    for i in range(len(p)):
+        hit = (Z == world[i]) 
+        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
+    s = sum(q)
+    for i in range(len(q)):
+        q[i] = q[i] / s
+    return q
+
+def move(p, U):
+    q = []
+    for i in range(len(p)):
+        s = pExact * p[(i-U) % len(p)]
+        s = s + pOvershoot * p[(i-U-1) % len(p)]
+        s = s + pUndershoot * p[(i-U+1) % len(p)]
+        q.append(s)
+    return q
